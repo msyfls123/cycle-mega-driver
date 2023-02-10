147c6fe6f896ff7f8a1a86442d3d9757a50fc29c
msyfls123 <msyfls123@gmail.com>
2023-02-11 01:39:12 +0800

    refactor: drop useless files

--- src/utils/browser.ts
+++
@@ -1,5 +0,0 @@
-import { BrowserWindow } from 'electron'
-
-export function checkBrowserAvailable (item?: BrowserWindow) {
-  return item instanceof BrowserWindow && !item.isDestroyed() && !item.webContents.isDestroyed()
-}
