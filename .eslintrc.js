ae197d2f13c14bd6b4e60ba013b33a3a9cc49b76
msyfls123 <msyfls123@gmail.com>
2023-02-11 02:00:15 +0800

    style: fix eslints

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,26 @@ module.exports = {
   },
   extends: 'standard-with-typescript',
   overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+      rules: {
+        '@typescript-eslint/comma-dangle': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'warn',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/consistent-type-assertions': 'off',
+        '@typescript-eslint/prefer-reduce-type-parameter': 'warn',
+        '@typescript-eslint/restrict-template-expressions': ['warn', { allowNumber: true }]
+      }
+    }
   ],
-  parserOptions: {
-    project: './tsconfig.json',
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
   rules: {
-    'semi': ['error', 'never'],
+    semi: ['error', 'never'],
     'comma-dangle': 'off',
-    '@typescript-eslint/comma-dangle': 'off',
-    "@typescript-eslint/strict-boolean-expressions": "warn",
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/consistent-type-assertions': 'off',
-    "@typescript-eslint/prefer-reduce-type-parameter": "warn",
-    '@typescript-eslint/restrict-template-expressions': ['warn', { allowNumber: true }]
   }
 }
--- example/watch.mjs
+++ example/watch.mjs
@@ -10,53 +10,53 @@ import { distDir } from './constants.mjs'
 import exampleConfigs from './rollup.config.mjs'
 
 const electron = connect.server.create({
-    spawnOpt: {
-        env: {
-            ...process.env,
-            DEBUG_COLORS: 1,
-            FORCE_COLOR: 'true',
-            DEBUG: '*',
-        },
+  spawnOpt: {
+    env: {
+      ...process.env,
+      DEBUG_COLORS: 1,
+      FORCE_COLOR: 'true',
+      DEBUG: '*',
     },
+  },
 })
 const args = [path.join(distDir, 'main.js')]
 
 const restart = _.debounce(() => new Promise((resolve) => {
-    electron.restart(args, (state) => {
-        if (state === 'restarted') {
-            resolve()
-        }
-    })
+  electron.restart(args, (state) => {
+    if (state === 'restarted') {
+      resolve()
+    }
+  })
 }), 300)
 
-function watchCompile(configs) {
-    const watcher = watch(configs)
-    let started = false
-    watcher.on('event', (event) => {
-        if (/error/i.test(event.code)) {
-            console.error(event)
-        }
-        if (event.code === 'END' && !started) {
-            // Start browser process
-            electron.start(args);
-            // Restart browser process
-            gulp.watch([
-                '**/*',
-                '!renderer/**/*'
-            ], { cwd: distDir }, (done) => {
-                restart().then(done)
-            })
-            // Reload renderer process
-            gulp.watch([
-                'renderer/**/*',
-            ], { cwd: distDir }, (done) => {
-                electron.reload()
-                // 3000ms is hard coded in electron-connect
-                setTimeout(done, 3000)
-            })
-            started = true
-        }
-    });
+function watchCompile (configs) {
+  const watcher = watch(configs)
+  let started = false
+  watcher.on('event', (event) => {
+    if (/error/i.test(event.code)) {
+      console.error(event)
+    }
+    if (event.code === 'END' && !started) {
+      // Start browser process
+      electron.start(args)
+      // Restart browser process
+      gulp.watch([
+        '**/*',
+        '!renderer/**/*'
+      ], { cwd: distDir }, (done) => {
+        restart().then(done)
+      })
+      // Reload renderer process
+      gulp.watch([
+        'renderer/**/*',
+      ], { cwd: distDir }, (done) => {
+        electron.reload()
+        // 3000ms is hard coded in electron-connect
+        setTimeout(done, 3000)
+      })
+      started = true
+    }
+  })
 }
 
 watchCompile([...libConfigs, ...exampleConfigs])
